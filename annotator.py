14d5f30049a3c1b98af9a186d21e59026b2147be
Yugnaynehc <cyyyyc123@hotmail.com>
2017-01-01 10:53:21 +0800

    try to add rect

--- annotator.py
+++ annotator.py
@@ -9,11 +9,14 @@
 # 4 -> Occlusion + Blur
 
 from __future__ import print_function
+import re
 import sys
 import os.path
 from PyQt4 import QtCore, QtGui, uic
 from PyQt4.QtCore import Qt
 import numpy as np
+from PIL import Image
+from PIL.ImageQt import ImageQt
 from scipy.io import loadmat, savemat
 
 
@@ -41,6 +44,7 @@ class MyApp(QtGui.QMainWindow, uiMainWindow):
         self.initControlButtons()
 
         # Init attribution data
+        self.seqAttrFile = None
         self.labels = None
 
         # Init ground-truth data
@@ -49,7 +53,7 @@ class MyApp(QtGui.QMainWindow, uiMainWindow):
     def initSeqList(self):
         self.seq_list = sorted(os.listdir(self.datasetRoot))
         for seq in self.seq_list:
-            if seq == 'anno':
+            if seq == 'anno' or seq == 'MEEM':
                 continue
             item = QtGui.QListWidgetItem(seq)
             self.seqList.addItem(item)
@@ -58,7 +62,7 @@ class MyApp(QtGui.QMainWindow, uiMainWindow):
 
     def initAnnotatorWidgets(self):
         self.imageRow = 3
-        self.imageCol = 4
+        self.imageCol = 3
         self.pageSize = self.imageRow * self.imageCol
         self.annotatorWidgets = []
         for i in range(self.imageRow):
@@ -80,13 +84,19 @@ class MyApp(QtGui.QMainWindow, uiMainWindow):
 
     def initSeq(self, item):
         '''
-        Init and show the first page of the selected sequence,
+        Init and show the first page of the current sequence,
         then read the attribution data of it.
         '''
         self.currentSeq = str(item.text())
+        # Read ground-truth of this sequence
+        self.readGTs()
+
+        # Prepare images of this sequence
         self.seqImgDir = os.path.join(self.datasetRoot, self.currentSeq, 'img')
         self.imgNames = sorted(os.listdir(self.seqImgDir))
         self.seqLen = len(self.imgNames)
+
+        # Init page indeies
         self.startIdx = 0
         self.prevPage.setEnabled(False)
         if self.seqLen <= self.pageSize:
@@ -114,18 +124,36 @@ class MyApp(QtGui.QMainWindow, uiMainWindow):
         # Show the attribution data
         self.showAttrData()
 
+    def readGTs(self):
+        '''
+        Read the ground-truth of current sequence.
+        '''
+        gtFilePath = os.path.join(
+            self.datasetRoot, self.currentSeq, 'groundtruth_rect.txt')
+        f = open(gtFilePath)
+        seq_pattern = r'[\d]+'
+        # origin gt format is [x, y, w, h]
+        self.gts = [map(int, re.findall(seq_pattern, line)) for line in list(f)]
+        # convert gt format to [x1, y1, x2, y2]
+        # self.gts = [[x, y, x + w, y + h] for (x, y, w, h) in self.gts]
+
     def showImages(self):
         '''
-        Show images of the selected sequence by start and end index
+        Show images of the current sequence by start and end index
         '''
         for i in range(self.startIdx, self.endIdx):
             imagePath = os.path.join(self.seqImgDir, self.imgNames[i])
-            self.annotatorWidgets[i - self.startIdx].setImage(imagePath)
+            bbox = self.gts[i]
+            self.annotatorWidgets[i - self.startIdx].setImage(imagePath, bbox)
             # frameID = int(self.imgNames[i].split('.')[0]) - 1
             self.annotatorWidgets[i - self.startIdx].setFrameID(i)
         self.update()
 
     def readAttrData(self):
+        '''
+        Read the attribution data from mat file.
+        If the file does not exist, init it by all 0.
+        '''
         self.seqAttrFile = os.path.join(self.attrRoot, self.currentSeq)
         try:
             attrData = loadmat(self.seqAttrFile)['label']
@@ -133,6 +161,7 @@ class MyApp(QtGui.QMainWindow, uiMainWindow):
             rawData = [label[0] for label in attrData]
             rawDataLen = len(rawData)
             if rawDataLen < self.seqLen:
+                # If some frame have not been annotated, then use 0 to fill it.
                 print('Only %d frames of sequence %s have been annotated before.' %
                       (rawDataLen, self.currentSeq))
                 print('Padding the attribution data by 0')
@@ -149,7 +178,7 @@ class MyApp(QtGui.QMainWindow, uiMainWindow):
 
     def showAttrData(self):
         '''
-        Show attribution data of the selected sequence by start and end index
+        Show attribution data of the current sequence by start and end index
         '''
         try:
             for i in range(self.startIdx, self.endIdx):
@@ -161,20 +190,25 @@ class MyApp(QtGui.QMainWindow, uiMainWindow):
 
     def saveAttrData(self):
         '''
-        Save attribution data of the selected sequence
+        Save attribution data of the current sequence
         '''
         try:
-            # Reshape the attribute list to matrix
-            saveData = np.array(self.labels).reshape(-1, 1)
-            # print(saveData)
-            target = self.seqAttrFile + '.mat'
-            savemat(target, {'label': saveData}, do_compression=True)
-            print('Save to %s\n' % target)
+            if self.seqAttrFile is not None:
+                # Reshape the attribute list to matrix
+                saveData = np.array(self.labels).reshape(-1, 1)
+                # print(saveData)
+                target = self.seqAttrFile + '.mat'
+                savemat(target, {'label': saveData}, do_compression=True)
+                print('Save to %s\n' % target)
         except Exception, e:
             print('When save attribution data, ')
             print(e)
 
     def showPrevPage(self):
+        '''
+        Calculate start and end index for previous page.
+        Be careful when reach the head point of the sequence.
+        '''
         self.endIdx = self.startIdx
         self.startIdx = self.startIdx - self.pageSize
         if self.startIdx <= 0:
@@ -189,6 +223,10 @@ class MyApp(QtGui.QMainWindow, uiMainWindow):
         self.showAttrData()
 
     def showNextPage(self):
+        '''
+        Calculate start and end index for next page.
+        Be careful when reach the tail point of the sequence.
+        '''
         self.startIdx = self.endIdx
         self.endIdx = self.startIdx + self.pageSize
         if self.endIdx > self.seqLen:
@@ -203,6 +241,9 @@ class MyApp(QtGui.QMainWindow, uiMainWindow):
         self.showAttrData()
 
     def saveAndQuit(self):
+        '''
+        Save the attribution changes and quit.
+        '''
         self.saveAttrData()
         self.close()
 
@@ -224,17 +265,28 @@ class MyApp(QtGui.QMainWindow, uiMainWindow):
 
 class ImageWidget(QtGui.QLabel):
 
-    def __init__(self, img=None):
+    def __init__(self, imagePath=None, bbox=None):
         super(ImageWidget, self).__init__()
         self.setFrameStyle(QtGui.QFrame.StyledPanel)
-        if img is not None:
-            self.setImage(img)
+        if imagePath is not None and bbox is not None:
+            self.setImage(imagePath, bbox)
         else:
             self.pixmap = None
             self.setText('No sequence selected')
 
-    def setImage(self, img):
-        self.pixmap = QtGui.QPixmap(img)
+    def setImage(self, imagePath, bbox):
+        '''
+        Read the image by PIL, and crop this image by input bounding box,
+        then covert the result to QPixmap format.
+        '''
+        # img = Image.open(imagePath)
+        # img = img.crop(bbox)
+        # self.pixmap = QtGui.QPixmap.fromImage(ImageQt(img))
+        self.pixmap = QtGui.QPixmap(imagePath)
+        self.bbox = bbox
+        # p = QtGui.QPainter(self.pixmap)
+        # p.drawRect(100, 100, 10, 10)
+        # p.end()
         # self.setPixmap(self.pixmap)
 
     def paintEvent(self, event):
@@ -243,6 +295,7 @@ class ImageWidget(QtGui.QLabel):
             return
         size = self.size()
         painter = QtGui.QPainter(self)
+        # painter.drawRect(*self.bbox)
         point = QtCore.QPoint(0, 0)
         scaledPix = self.pixmap.scaled(size, Qt.KeepAspectRatio,
                                        transformMode=Qt.SmoothTransformation)
@@ -268,18 +321,22 @@ class AnnotatorWidget(QtGui.QWidget):
         self.buttonGroup = QtGui.QButtonGroup()
         self.buttons = []
         labels = ['O', 'D', 'B', 'OB']
+        label_colors = ['LightCoral', 'Chartreuse', 'Aquamarine', 'CadetBlue']
         for i in range(0, 4):
             button = QtGui.QRadioButton(labels[i])
+            button.setStyleSheet('QRadioButton:checked { background-color: %s }'
+                                 % label_colors[i])
             button.setCheckable(False)
             self.buttons.append(button)
             self.buttonLayout.addWidget(button)
             self.buttonGroup.addButton(button, i + 1)
+        self.buttonGroup.buttonClicked.connect(self.attrSelected)
+
+        # Tight all widgets together
         self.layout.addWidget(self.imageWidget)
         self.layout.addLayout(self.buttonLayout)
         self.layout.setStretch(1, 6)
         self.setLayout(self.layout)
-
-        self.buttonGroup.buttonClicked.connect(self.attrSelected)
 
     def setFrameID(self, frameID):
         self.frameID = frameID
@@ -290,8 +347,8 @@ class AnnotatorWidget(QtGui.QWidget):
         '''
         self.labels = labels
 
-    def setImage(self, img):
-        self.imageWidget.setImage(img)
+    def setImage(self, imagePath, bbox):
+        self.imageWidget.setImage(imagePath, bbox)
 
     def setAttr(self, attr):
         if attr != 0:
@@ -308,12 +365,18 @@ class AnnotatorWidget(QtGui.QWidget):
         self.buttonGroup.setExclusive(True)
 
     def attrSelected(self):
-        print(self.frameID, self.buttonGroup.checkedId())
-        self.labels[self.frameID] = self.buttonGroup.checkedId()
+        '''
+        When some radio button is checked, change the attribution of the
+        corresponding frame.
+
+        '''
+        # print(self.frameID, self.buttonGroup.checkedId())
+        if self.labels is not None:
+            self.labels[self.frameID] = self.buttonGroup.checkedId()
 
 if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
-    dataRoot = 'f:\otb'
+    dataRoot = '/home/feather/Dataset/otb'
     window = MyApp(dataRoot)
     window.show()
     sys.exit(app.exec_())
